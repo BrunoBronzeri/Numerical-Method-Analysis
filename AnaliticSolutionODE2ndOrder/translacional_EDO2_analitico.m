36656cd26503d81740466cac2dc7192bf1f29dbf
Bruno Bronzeri <83321523+BrunoBronzeri@users.noreply.github.com>
2022-11-25 21:57:51 -0300

    Update translacional_EDO2_analitico.m

--- AnaliticSolutionODE2ndOrder/translacional_EDO2_analitico.m
+++ AnaliticSolutionODE2ndOrder/translacional_EDO2_analitico.m
@@ -7,7 +7,7 @@ M = 1;
 K = 2;
 B = 0.5;
 
-########## Par�metros de interesse ###########
+########## Parâmetros de interesse ###########
 wn = sqrt(K/M)
 csi = B/M*sqrt(M/K)*1/2
 Kcc = 1/K
@@ -20,26 +20,26 @@ ta = 5/tau
 
 fa = @(t) 1; % entrada
 t = [0:1e-3:40];
-xo = [0;0] % condi��o inicial
+xo = [0;0] % condição inicial
 
 % ODEs
-% montando modelo de espa�o de estados
+% montando modelo de espaço de estados
 dx1 = @(t,x) x(2);
 dx2 = @(t,x) -B/M*x(2) -K/M*x(1)+fa(t)/M;
 
 dxdt = @(t,x) [dx1(t,x);dx2(t,x)];
 
-%%% Solu��o Sistema
+%%% Solução Sistema
 
 [t,x] = ode45(dxdt, t, xo);
 
 figure
 plot(t, x, 'linewidth', 2)
-legend('Posi��o (m)', 'Velocidade (m/s)')
+legend('Posição (m)', 'Velocidade (m/s)')
 xlabel('Time')
 grid
 
-% 13.389 --> 17.856  per�odo da sen�ide T = 4,467
+% 13.389 --> 17.856  período da senóide T = 4,467
 % freq. --> 0,2239
 % wd = 2*pi*f --> 1,407
-% Portanto os polos de sistema ser�o: P = alpha +/- j*betha %%% P1,2 = 5/40 +/- j*1,407 %%%
+% Portanto os polos de sistema serão: P = alpha +/- j*betha %%% P1,2 = 5/40 +/- j*1,407 %%%
